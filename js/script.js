57d5aef47c5939510c396d0d92181dc416a24cc1
Serdiuk-Roman <serdiuk.r@gmail.com>
2023-02-16 20:56:18 +0200

    update mobile version, add scroll top

--- js/script.js
+++ js/script.js
@@ -11,6 +11,32 @@ for (i = 0; i < coll.length; i++) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
+}
+
+// Burger button
+function button_burger(item) {
+  item.classList.toggle("change");
+}
+
+
+// Get the button
+let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
